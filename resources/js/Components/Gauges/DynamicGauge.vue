d33621474a062d0dd14711bdb994f80113dba02f
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2024-01-16 12:58:18 +0100

    Edit DynamicGauge component

--- resources/js/Components/Gauges/DynamicGauge.vue
+++ resources/js/Components/Gauges/DynamicGauge.vue
@@ -2,10 +2,10 @@
     <div class="flex justify-start mt-2 flex-wrap">
         <div v-for="(item, index) in max" :key="index">
             <template v-if="index <= selectedPoints">
-                <i :class="`mx-1 text-2xl ${icon} ${color}`" @click="setValue(index)"></i>
+                <i :class="`mx-1 text-3xl ${icon} ${color}`" @click="setValue(index)"></i>
             </template>
             <template v-else>
-                <i :class="`mx-1 text-2xl ${icon} text-gray-400`" @click="setValue(index)"></i>
+                <i :class="`mx-1 text-3xl ${icon} text-gray-400`" @click="setValue(index)"></i>
             </template>
         </div>
     </div>
@@ -13,7 +13,7 @@
 
 <script setup>
 import { onMounted, ref } from "vue";
-import {useCharacterStore} from "@/Stores/characterStore.js";
+import { useCharacterStore } from "@/Stores/characterStore.js";
 import { useCharacterAttributesStore } from "@/Stores/characterAttributesStore.js";
 
 const characterStore = useCharacterStore();
