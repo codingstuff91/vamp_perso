d06955e5026b37d50bdeb1d5569b516236c4a2be
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2024-01-22 08:30:25 +0100

    Feat display description for Powers

--- resources/js/Pages/Character/Partials/Power.vue
+++ resources/js/Pages/Character/Partials/Power.vue
@@ -1,12 +1,20 @@
 <script setup>
+import { useModalStore } from "@/Stores/modalStore.js";
+const modalStore = useModalStore();
+
 defineProps({
     power : Object
 })
+
+const showDescription = async (entity, id) => {
+    await modalStore.getDescription(entity, id);
+    await modalStore.setModalStatus();
+}
 </script>
 
 <template>
     <div class="px-2 py-4 mx-2 my-2 flex justify-between bg-gray-800 border border-red-500">
-        <p class="text-white">{{ power.name }}</p>
+        <p class="text-white" @click="showDescription('power', power.id)">{{ power.name }}</p>
         <p class="text-red-500">{{ power.dice_pool }}</p>
         <p class="text-white">Lvl 1</p>
     </div>
