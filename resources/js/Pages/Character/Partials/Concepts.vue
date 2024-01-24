bd053bc900a9fa3ed817585c387f9a50bcccd731
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2024-01-24 09:19:07 +0100

    Add description modal into concepts component

--- resources/js/Pages/Character/Partials/Concepts.vue
+++ resources/js/Pages/Character/Partials/Concepts.vue
@@ -1,21 +1,37 @@
 <script setup>
 import { useCharacterConceptsStore } from "@/Stores/characterConceptsStore.js";
-import { useCharacterStore } from "@/Stores/characterStore.js";
+import { useModalStore } from "@/Stores/modalStore.js";
+
+import DescriptionModal from "@/Components/Modals/DescriptionModal.vue";
+
 const conceptsStore = useCharacterConceptsStore();
-const characterStore = useCharacterStore();
+const modalStore = useModalStore();
 
 const formatTitle = (title) => {
     return title.charAt(0).toUpperCase() + title.slice(1);
 }
 
+const showDescription = async (entity, id) => {
+    await modalStore.getDescription(entity, id);
+    await modalStore.setModalStatus();
+}
 </script>
 
 <template>
     <div class="py-2 px-4">
+        <transition name="fade">
+            <DescriptionModal v-if="modalStore.open" />
+        </transition>
+
         <h1 class="section_title text-center mt-4">Concepts</h1>
         <div class="flex flex-col items-center">
             <div v-for="concept in conceptsStore.concepts">
-                <h2 class="attribute_title mt-6 text-center">{{ formatTitle(concept.name) }}</h2>
+                <h2
+                    class="attribute_title mt-6 text-center"
+                    @click="showDescription('concept', concept.id)"
+                >
+                    {{ formatTitle(concept.name) }}
+                </h2>
                 <p class="subtitle mt-2">{{ concept.pivot.concept_value }}</p>
             </div>
         </div>
