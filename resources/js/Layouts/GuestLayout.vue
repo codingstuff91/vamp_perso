5a2b37b9b9c5df9821f00c7d88c945709e426e84
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2024-01-27 22:55:30 +0100

    Add image logo and favicon

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -5,7 +5,11 @@ import { Link } from '@inertiajs/vue3';
 
 <template>
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-darkness-900">
-        <div>
+        <div class="flex justify-center items-center">
+            <img
+                src="/img/dracula.png"
+                class="rounded-full w-20 h-20 mr-2"
+            >
             <h1 class="main_title">VAMP PERSO</h1>
         </div>
 
