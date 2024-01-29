0f3b242a2ba3cd353ab7d9ea9808a2fdccb75a9e
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2024-01-29 13:23:21 +0100

    Edit style of description modal text

--- resources/js/Components/Modals/DescriptionModal.vue
+++ resources/js/Components/Modals/DescriptionModal.vue
@@ -3,16 +3,18 @@
         class="bg-darkness-900 absolute left-0 top-[64px] w-full h-full overflow-y-auto opacity-100 transition-opacity z-50 border-t border-blood-500"
         v-if="modalStore.open"
     >
-        <button
-            class="fixed right-4 top-[77px] rounded-full border-2 border-blood-500 w-10 h-10"
-            @click="modalStore.setModalStatus"
-        >
-            <i class="text-blood-500 text-2xl fa-solid fa-xmark"></i>
-        </button>
+        <div class="w-full mx-auto lg:max-w-7xl lg:mx-auto">
+            <button
+                class="fixed right-4 top-[77px] rounded-full border-2 border-blood-500 w-10 h-10"
+                @click="modalStore.setModalStatus"
+            >
+                <i class="text-blood-500 text-2xl fa-solid fa-xmark"></i>
+            </button>
 
-        <h1 class="attribute_category_title pt-8 text-blood-500 text-center">{{ modalStore.details.name }}</h1>
+            <h1 class="attribute_category_title pt-8 text-blood-500 text-center">{{ modalStore.details.name }}</h1>
 
-        <p class="subtitle text-justify px-4" v-html="modalStore.details.description.text"></p>
+            <p class="subtitle text-justify px-4" v-html="modalStore.details.description.text"></p>
+        </div>
     </div>
 </template>
 
