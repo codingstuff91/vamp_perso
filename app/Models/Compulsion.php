967a80e85f8f30e50aea74b32c487819aad132e4
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2024-03-05 08:28:48 +0100

    Add relationship between Compulsion and Clan models

--- app/Models/Compulsion.php
+++ app/Models/Compulsion.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Compulsion extends Model
 {
@@ -12,4 +13,9 @@ class Compulsion extends Model
     public $timestamps = false;
 
     protected $guarded = [];
+
+    public function clan(): BelongsTo
+    {
+        return $this->belongsTo(Clan::class);
+    }
 }
