9a58fbd09228a8791a5054942e4d93927e5d1ba2
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2024-03-07 09:30:29 +0100

    Create Action class to get compulsions lists and unit test

---
+++ tests/Unit/Actions/GetCompulsionsListActionTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\Character;
+use App\Models\User;
+
+test('renders the whole compulsions list including the character\'s clan specific compulsion', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    $this->seed([
+        ClanSeeder::class,
+        CompulsionSeeder::class,
+        CharacterSeeder::class,
+    ]);
+
+    $character = Character::first();
+
+    $compulsions = app(App\Actions\Compulsions\GetCompulsionsListAction::class)->execute($character);
+
+    /** Whe should receive 4 generic compulsions + 1 clan's specific compulsion */
+    expect($compulsions->count())->toBe(5);
+});
