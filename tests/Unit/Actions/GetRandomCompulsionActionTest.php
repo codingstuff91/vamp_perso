9f9764065362f77e779ba806579f3e38ad992e22
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2024-03-09 14:40:59 +0100

    Edit test

--- tests/Unit/Actions/GetRandomCompulsionActionTest.php
+++ tests/Unit/Actions/GetRandomCompulsionActionTest.php
@@ -1,12 +1,14 @@
 <?php
 
+use App\Actions\Compulsions\GetRandomCompulsionAction;
 use App\Models\Character;
+use App\Models\Compulsion;
 use App\Models\User;
 use Database\Seeders\CharacterSeeder;
 use Database\Seeders\ClanSeeder;
 use Database\Seeders\CompulsionSeeder;
 
-test('renders the whole compulsions list including the character\'s clan specific compulsion', function () {
+test('get a random compulsion from a list including the character\'s clan compulsion', function () {
     $user = User::factory()->create();
     $this->actingAs($user);
 
@@ -18,8 +20,7 @@ test('renders the whole compulsions list including the character\'s clan specifi
 
     $character = Character::first();
 
-    $compulsions = app(App\Actions\Compulsions\GetCompulsionsListAction::class)->execute($character);
+    $compulsion = app(GetRandomCompulsionAction::class)->execute($character);
 
-    /** Whe should receive 4 generic compulsions + 1 clan's specific compulsion */
-    expect($compulsions->count())->toBe(5);
+    expect($compulsion)->toBeInstanceOf(Compulsion::class);
 });
