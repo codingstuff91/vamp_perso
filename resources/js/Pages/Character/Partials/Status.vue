5468cd50345f48d19fbe440574bf44b246a7eace
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2024-03-12 09:06:04 +0100

    Apply new modal on Disciplines and Status

--- resources/js/Pages/Character/Partials/Status.vue
+++ resources/js/Pages/Character/Partials/Status.vue
@@ -5,38 +5,32 @@ import Humanity from "@/Pages/Character/Partials/Status/Humanity.vue";
 import Hunger from "@/Pages/Character/Partials/Status/Hunger.vue";
 import DescriptionModal from "@/Components/Modals/DescriptionModal.vue";
 
-import { useModalStore } from "@/Stores/modalStore.js";
+import {useModalStore} from "@/Stores/modalStore.js";
+import DetailsModal from "@/Components/Modals/DetailsModal.vue";
+
 const modalStore = useModalStore();
 
 const showDescription = async (entity, id) => {
     await modalStore.getDescription(entity, id);
-    await modalStore.setModalStatus();
+    await modalStore.toggle();
 }
 </script>
 
 <template>
     <div class="py-2">
-        <transition name="fade">
-            <DescriptionModal v-if="modalStore.open" />
-        </transition>
+        <DetailsModal
+            :show="modalStore.open"
+            :closeable="true"
+        />
 
-        <Hunger @get-description="showDescription" />
+        <Hunger @get-description="showDescription"/>
 
         <div class="mt-8 pt-4 grid grid-cols-1 gap-2 lg:grid-cols-3 lg:border-t lg:border-gray-700">
-            <Willpower @get-description="showDescription" />
+            <Willpower @get-description="showDescription"/>
 
-            <Health @get-description="showDescription" />
+            <Health @get-description="showDescription"/>
 
-            <Humanity @get-description="showDescription" />
+            <Humanity @get-description="showDescription"/>
         </div>
     </div>
 </template>
-
-<style scoped>
-.fade-enter-active, .fade-leave-active {
-    transition: opacity 0.5s;
-}
-.fade-enter, .fade-leave-to {
-    opacity: 0;
-}
-</style>
