80ee41c86772d2470e9f22a4474d0068fff4538b
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2024-03-14 09:01:57 +0100

    Improve gauge spacing and sizes

--- resources/js/Components/Gauges/AttributeGauge.vue
+++ resources/js/Components/Gauges/AttributeGauge.vue
@@ -2,17 +2,24 @@
     <div class="flex justify-start flex-wrap">
         <div v-for="(item, index) in max" :key="index">
             <template v-if="index <= selectedPoints">
-                <i :class="`mx-1 text-lg ${icon} ${color}`" @click="setValue(index)"></i>
+                <i
+                    :class="`mx-0.5 text-xl ${icon} ${color} lg:mx-1 lg:text-2xl`"
+                    @click="setValue(index)"
+                >
+                </i>
             </template>
             <template v-else>
-                <i :class="`mx-1 text-lg ${icon} text-gray-400`" @click="setValue(index)"></i>
+                <i :class="`mx-0.5 text-xl ${icon} text-gray-400 lg:mx-1 lg:text-2xl`"
+                   @click="setValue(index)"
+                >
+                </i>
             </template>
         </div>
     </div>
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
+import {onMounted, ref} from "vue";
 
 let remainingPoints = ref(0);
 let selectedPoints = ref(0);
@@ -39,7 +46,7 @@ const props = defineProps({
     },
 });
 const setValue = (index) => {
-    if (! props.editable) {
+    if (!props.editable) {
         return false;
     }
 
