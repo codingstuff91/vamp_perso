c860b8580c8596f181f49f5324de9cf0d281b2a5
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2024-03-17 20:20:22 +0100

    Edit migration

--- database/migrations/2024_03_17_115849_create_backgrounds_table.php
+++ database/migrations/2024_03_17_115849_create_backgrounds_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('backgrounds', function (Blueprint $table) {
             $table->id();
             $table->string('category');
-            $table->unsignedSmallInteger('type_id')->constrained();
+            $table->unsignedSmallInteger('background_type_id')->constrained();
             $table->tinyInteger('level');
             $table->string('name', 20);
             $table->text('notes', 200);
