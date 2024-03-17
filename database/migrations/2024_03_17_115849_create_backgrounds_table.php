6fba92eb028e7346e9c88d1476961483e2521cdf
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2024-03-17 13:23:51 +0100

    Add files for Backgrounds and background types

---
+++ database/migrations/2024_03_17_115849_create_backgrounds_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('backgrounds', function (Blueprint $table) {
+            $table->id();
+            $table->string('category');
+            $table->unsignedSmallInteger('type_id')->constrained();
+            $table->tinyInteger('level');
+            $table->string('name', 20);
+            $table->text('notes', 200);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('backgrounds');
+    }
+};
