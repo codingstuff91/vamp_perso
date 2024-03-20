f0524d5af60b1f3d15e0a5a5406b0bc084f9a6c9
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2024-03-20 09:10:33 +0100

    Make description relationship

--- app/Models/Background.php
+++ app/Models/Background.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 class Background extends Model
 {
@@ -14,5 +15,10 @@ class Background extends Model
     public function type()
     {
         return $this->belongsTo(BackgroundType::class, 'background_type_id');
+    }
+
+    public function description(): MorphOne
+    {
+        return $this->morphOne(Description::class, 'descriptionable');
     }
 }
