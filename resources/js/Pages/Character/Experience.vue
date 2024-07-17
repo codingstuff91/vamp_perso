866960fc3f80532bb6d228fc41fb53aa7e9dd1a2
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2024-07-17 13:02:48 +0200

    Add XP rules

--- resources/js/Pages/Character/Experience.vue
+++ resources/js/Pages/Character/Experience.vue
@@ -23,9 +23,16 @@ defineProps({
 
         <div class="pb-4">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="flex justify-center mx-4 my-4 px-4 py-2 bg-gray-600 rounded-lg">
-                    <h1>Attribution des points d'experience</h1>
+                <div class="my-4 flex flex-col items-center">
+                    <h1 class="section_title text-2xl">Conditions Attribution des XP</h1>
+                    <ul class="mt-2">
+                        <li class="my-2 text-skin-50 text-xl">1 pt pour le roleplay</li>
+                        <li class="my-2 text-skin-50 text-xl">1 pt si le PJ a appris quelque chose</li>
+                        <li class="my-2 text-skin-50 text-xl">1 pt si action majeure accomplie</li>
+                        <li class="my-2 text-skin-50 text-xl">1 pt si le PJ a suivi ses valeurs</li>
+                    </ul>
                 </div>
+
                 <div
                     v-for="character in characters" :key="character.id"
                     class="grid grid-cols-4"
