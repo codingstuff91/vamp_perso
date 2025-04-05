61494d8ef7ce6c66060f2a11852219d7b892b929
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2025-04-05 21:46:19 +0200

    Add feature test to update an attribute value

--- tests/Feature/Http/Controllers/AttributeImprovementControllerTest.php
+++ tests/Feature/Http/Controllers/AttributeImprovementControllerTest.php
@@ -24,3 +24,59 @@ test('The index page is rendered', function () {
 
     $response->assertStatus(200);
 });
+
+test('Decrease experience points from a character', function () {
+    $user = createUser();
+    $this->actingAs($user);
+
+    $character = createCharacter($user);
+
+    $attribute = Attribute::factory()->create();
+
+    AttributeCharacter::create([
+        'character_id' => $character->id,
+        'attribute_id' => $attribute->id,
+        'attribute_value' => 1,
+    ]);
+
+    $this->put(route('attribute_improve.update', [
+        'character' => $character->id,
+        'attribute' => $attribute->id,
+    ]), [
+        'newScore' => 2,
+        'consumedExperiencePoints' => 8,
+    ]);
+
+    $character->refresh();
+
+    expect($character->experience_points)->toBe(2);
+});
+
+test('Increase attribute value', function () {
+    $user = createUser();
+    $this->actingAs($user);
+
+    $character = createCharacter($user);
+
+    $attribute = Attribute::factory()->create();
+
+    AttributeCharacter::create([
+        'character_id' => $character->id,
+        'attribute_id' => $attribute->id,
+        'attribute_value' => 1,
+    ]);
+
+    $this->put(route('attribute_improve.update', [
+        'character' => $character->id,
+        'attribute' => $attribute->id,
+    ]), [
+        'newScore' => 2,
+        'consumedExperiencePoints' => 8,
+    ]);
+
+    $attributeCharacter = AttributeCharacter::where('character_id', $character->id)
+        ->where('attribute_id', $attribute->id)
+        ->first();
+
+    expect($attributeCharacter->attribute_value)->toBe(2);
+});
