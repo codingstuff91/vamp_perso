b1b4cfcdd648bc37b10a33cf78007fbefe22e7d2
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2025-04-05 23:22:01 +0200

    Remove abort condition

--- app/Http/Controllers/Character/AttributeImprovementController.php
+++ app/Http/Controllers/Character/AttributeImprovementController.php
@@ -29,8 +29,6 @@ class AttributeImprovementController extends Controller
 
         $requiredExperiencePoints = $this->attributeService->getRequiredExperiencePoints($attributeValue + 1, $attribute);
 
-        abort_if($requiredExperiencePoints > $character->experience_points, 403, __('Pas assez de points pour améliorer cet attribut.'));
-
         return Inertia::render('Character/AttributeImprovement', [
             'character' => $character,
             'attribute' => $attribute,
