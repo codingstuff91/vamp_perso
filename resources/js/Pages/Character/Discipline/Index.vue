82da6b0cf0d21e9b1975440a7172ecfaf0a929ce
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2025-04-07 11:16:45 +0200

    Select a discipline, show discipline description and show power description

--- resources/js/Pages/Character/Discipline/Index.vue
+++ resources/js/Pages/Character/Discipline/Index.vue
@@ -3,25 +3,35 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import {Head} from '@inertiajs/vue3';
 
 import {useDisciplineStore} from "@/Stores/disciplineStore.js";
+import {useModalStore} from "@/Stores/modalStore.js";
 import {ref} from "vue";
 import Power from "@/Pages/Character/Partials/Power.vue";
 import DetailsModal from "@/Components/Modals/DetailsModal.vue";
-import {useModalStore} from "@/Stores/modalStore.js";
 
 const disciplineStore = useDisciplineStore();
-
 const modalStore = useModalStore();
-
 
 const props = defineProps({
     disciplines: Array,
 });
 
 const powers = ref([]);
+const selectedDiscipline = ref(null);
+const disciplineDescription = ref(null);
 
 const getPowers = async (discipline) => {
     await disciplineStore.getPowers(discipline);
     powers.value = disciplineStore.powers;
+
+    selectDiscipline(discipline);
+};
+
+const selectDiscipline = (discipline) => {
+    disciplineDescription.value = props.disciplines.filter(element => {
+        return element.id === discipline.id
+    })[0].description;
+
+    selectedDiscipline.value = discipline.id;
 };
 </script>
 
@@ -44,19 +54,28 @@ const getPowers = async (discipline) => {
 
         <div class="pb-4">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <h1 class="text-center section_title my-4">Choisir une catégorie</h1>
+                <h1 class="text-center section_title my-4">Choisissez une discipline</h1>
 
-                <div class="flex justify-between mt-8">
+                <div class="grid grid-cols-3 gap-4 mt-8">
                     <div
                         v-for="discipline in disciplines"
                         :key="discipline.id"
-                        class="py-2 px-4 bg-blood-500 rounded-lg"
+                        :class="{'desktop_menu_active' : selectedDiscipline === discipline.id}"
+                        class="py-2 px-4 rounded-lg attribute_title"
                         @click="getPowers(discipline)"
                     >
-                        <h2 class="text-white text-xl font-bold">
+                        <h2 class="">
                             {{ discipline.name }}
                         </h2>
                     </div>
+                </div>
+
+                <div
+                    v-if="disciplineDescription"
+                    class="my-4 subtitle text-justify"
+                >
+                    <h3 class="column_title my-4">Description</h3>
+                    {{ disciplineDescription }}
                 </div>
 
                 <div class="grid grid-cols-3 gap-x-4">
