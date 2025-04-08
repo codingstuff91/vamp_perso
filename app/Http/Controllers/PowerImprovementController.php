a5dafa2825e9358aee1ce4d6f97aa16347525467
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2025-04-08 17:56:02 +0200

    test

--- app/Http/Controllers/PowerImprovementController.php
+++ app/Http/Controllers/PowerImprovementController.php
@@ -17,6 +17,8 @@ class PowerImprovementController extends Controller
 
     public function index(Character $character)
     {
+        dd($character->clan);
+
         return Inertia::render('Character/Discipline/Index', [
             'disciplines' => Discipline::all(),
             'characterPowers' => $character->powers()->get()->pluck('id'),
